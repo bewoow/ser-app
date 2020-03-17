6a8e06fb2ba01bb88362abc19b7beb10913661ff
Nikos Karamolegkos <nikolaos@protonmail.com>
2020-03-17 19:55:54 -0400

    Update requirements file.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 printf "->Creating the virtual environment\n"
-virtualenv ~/.venv-tdi-capstone
+virtualenv --python=python3 ~/.venv-tdi-capstone
 
 printf "->Activating the virtual environment\n"
 source ~/.venv-tdi-capstone/bin/activate
 
 printf "->Installing Python modules\n"
-pip install -r reqs.txt
-python -m pip install jupyter
+pip3 install -r reqs.txt
+python3 -m pip install jupyter
 ipython kernel install --user --name=.venv-tdi-capstone
 
 source ~/.venv-tdi-capstone/bin/activate
