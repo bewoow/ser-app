6f98a5f63a02b43345bf41b93d3973abf86315b0
Nikos Karamolegkos <nikolaos@protonmail.com>
2020-03-18 14:57:07 -0400

    Update single subject analysis.

--- analysis/se_analysis_single subject.py
+++ analysis/se_analysis_single subject.py
@@ -12,157 +12,9 @@ import joblib
 # sys.path.append(os.path.abspath('../app'))
 
 os.chdir('../app/')
-from lib import get_se_cohort, get_chart_data, create_bokeh_viz_chartdata
+from lib import get_se_cohort, get_chart_data, create_bokeh_viz_chartdata, \
+    predict_ser_risk, impute_values, compute_total_gcs, compute_rsbi_ve, compute_ser_features, get_ser_analysis_df
 os.chdir('../')
-
-def impute_values(chart_data):
-    """Impute chart daata
-
-    Arguments:
-        chart_data {chart_data} -- [description]
-
-    Returns:
-        chart_data_imputed -- Imputed chart data
-    """
-    chart_data = chart_data.dropna(subset=['vital_sign'])
-
-    # Get vital signs available in patient data.
-    vital_signs = chart_data['vital_sign'].unique()
-
-    # Extract the unique charttimes that will be imputed if not present.
-    charttimes = chart_data['charttime'].unique()
-
-    # For all vital signs, check charttimes and interpolate if missing.
-    chart_data_imputed = pd.DataFrame(columns=chart_data.columns)
-    for vs in vital_signs:
-        df = chart_data[chart_data['vital_sign'] == vs]
-
-        # Take the average value of entries with the same charttime. We use the
-        # 'max' for all other entries (e.g., units, etc) so that the aggregated
-        # dataframe includes all entries.
-        group_by_agg = {column: 'max' for column in df.columns if column != 'charttime'}
-        group_by_agg['valuenum'] = 'mean'
-        df = df.groupby('charttime', as_index=False).agg(group_by_agg)
-
-        # If a charttime from the charttimes of the vital sign with most
-        # entries does not exist, create new row in the dataframe
-        # for charttime in charttimes:
-        df_2 = pd.DataFrame(columns=chart_data.columns)
-        df_2['charttime'] = list(charttimes)
-        df = df.append(df_2).drop_duplicates(['charttime'])
-
-        # Apply linear interpolation on the added charttimes. Note that
-        # 'icustay_id', 'subject_id', 'unit', and 'vital_sign' should all the
-        # same. However, 'unit' and 'vital_sign' are categorical data that get
-        # NaN from the interpolation and hence we need to fill them.
-        df = df.sort_values(by='charttime').\
-            interpolate(method='linear', limit_direction='both', axis=0).\
-            fillna(method='ffill').fillna(method='bfill')
-        # Convert 'icustay_id', 'subject_id' back to integer values.
-        df['icustay_id'] = df['icustay_id'].apply(int)
-        df['subject_id'] = df['subject_id'].apply(int)
-
-        chart_data_imputed = chart_data_imputed.append(df, sort=True, ignore_index=True)
-
-    return chart_data_imputed.dropna(subset=['icustay_id'])
-
-def compute_total_gcs(chart_data):
-    """Computes the total Glasgow Coma Scale (GCS) score as the sum of the
-    individual components (eye, motor, verbal).
-
-    Arguments:
-        chart_data {pd.DataFrame} -- Patient data in a Pandas dataframe.
-
-    Returns:
-        pd.DataFrame -- Patient data dataframe with total GCS (GCStot) included.
-    """
-    # Get vital signs available in patient data.
-    vital_signs = chart_data['vital_sign'].unique()
-
-    # We verify that all the GCS individual components are part of the data.
-    if all([gcs_comp in vital_signs for gcs_comp in ('GCSmotor', 'GCSeye', 'GCSverbal')]):
-        # Extract individual GCS component scores.
-        GCSmotor = chart_data[chart_data['vital_sign'] == 'GCSmotor'].set_index('charttime')
-        GCSeye = chart_data[chart_data['vital_sign'] == 'GCSeye'].set_index('charttime')
-        GCSverbal = chart_data[chart_data['vital_sign']
-                                 == 'GCSverbal'].set_index('charttime')
-
-        # Create a new dataframe and update 'vital_sign' and 'valuenum' columns.
-        df_gcs = pd.DataFrame(columns=GCSmotor.columns)
-
-        df_gcs['valuenum'] = (GCSmotor['valuenum'] + GCSeye['valuenum'] + \
-            GCSverbal['valuenum'])
-        df_gcs = df_gcs.dropna(subset=['valuenum'])
-        df_gcs['vital_sign'] = 'GCStot'
-        df_gcs['unit'] = None
-        df_gcs['subject_id'] = GCSmotor['subject_id']
-        df_gcs['icustay_id'] = GCSmotor['icustay_id']
-
-        # Append to chart_data dataframe.
-        chart_data = chart_data.append(df_gcs.reset_index(), sort=True, ignore_index=True)
-
-    # If total GCS exists in patient data, just return
-    return chart_data
-
-def compute_rsbi_ve(chart_data):
-    """Compute and append RSBI (rapid shallow breathing index) and VE (minute
-    ventilation) on the 'chart_data' dataframe
-
-    Arguments:
-        chart_data {pd.DataFrame} -- patient data
-
-    Returns:
-        pd.DataFrame -- chart_data
-    """
-    vital_signs = chart_data['vital_sign'].unique()
-    def create_rsbi_ve_dfs(vt_name):
-        """Create dataframes for RSBI and VE based on provided tidal volume
-        name (VTobs, VTspot)
-
-        Arguments:
-            vt_name {string} -- Name of VT dataframe to be used (VTspot, VTobs)
-
-        Returns:
-            pd.DataFrame -- chart_data that includes RSBI and VE
-        """
-        RR = chart_data[chart_data['vital_sign']
-                          == 'RR'].set_index('charttime')
-        VTspot = chart_data[chart_data['vital_sign']
-                          == 'VTspot'].set_index('charttime')
-        VTobs = chart_data[chart_data['vital_sign']
-                         == 'VTobs'].set_index('charttime')
-
-        rsbi = RR.copy()
-        ve = RR.copy()
-
-        # Get appropriate tidal volume (obs or spot)
-        VT = eval(vt_name + "['valuenum'] / 1000")
-
-        rsbi['valuenum'] = RR['valuenum'] / VT
-        rsbi['unit'] = 'bpm/l'
-        rsbi['vital_sign'] = 'RSBI' + vt_name.split('VT')[1]
-
-        ve['valuenum'] = RR['valuenum'] * VT
-        ve['unit'] = 'min/l'
-        ve['vital_sign'] = 'VE' + vt_name.split('VT')[1]
-
-        # Reset indices and append to 'chart_data' dataframe.
-        rsbi = rsbi.reset_index()
-        pat_dat = chart_data.append(rsbi, sort=True, ignore_index=True)
-
-        ve = ve.reset_index()
-        pat_dat = pat_dat.append(ve, sort=True, ignore_index=True)
-
-        return pat_dat
-
-    # Compute and append RSBI and Ve for observed and spontaneous tidal volumes.
-    if 'VTobs' in vital_signs:
-        chart_data = create_rsbi_ve_dfs('VTobs')
-    if 'VTspot' in vital_signs:
-        chart_data = create_rsbi_ve_dfs('VTspot')
-
-    return chart_data
-
 
 # %%
 df_se_cohort_reduced = get_se_cohort()
@@ -223,6 +75,8 @@ while seg_end_time > sorted_charttimes[0]:
 
         df_se_analysis.loc[i, 'label'] = label_idx
         df_se_analysis.loc[i, 'icustay_id'] = int(df['icustay_id'].iloc[0])
+        df_se_analysis.loc[i, 'seg_start_time'] = seg_start_time
+        df_se_analysis.loc[i, 'seg_end_time'] = seg_end_time
         for func in functions:
             if func == 'mode':
                 df_se_analysis.loc[i, '{0}_{1}'.format(vital_sign, func)] = \
@@ -252,7 +106,7 @@ df_se_data_trun = pd.concat([df_se_analysis.loc[group_idx]
                              for i, group_idx in segment_groups.groups.items() if i <= num_segments])
 
 # Use only the features in the training dataset
-columns_to_keep = ['label', 'icustay_id']
+columns_to_keep = ['label', 'icustay_id', 'seg_start_time', 'seg_end_time']
 columns_to_keep.extend(list(df_se_data_population_mean.index.values))
 df_se_data_trun = df_se_data_trun[columns_to_keep]
 
@@ -262,7 +116,7 @@ segment_groups_trun = df_se_data_trun.groupby('label')
 feature_count_seg = segment_groups_trun.count()
 
 for feat in feature_count_seg.columns:
-    if feat != 'icustay_id':
+    if feat not in ('icustay_id', 'seg_start_time', 'seg_end_time'):
         # Impute nan feature values with their averages
         for label, label_idx in segment_groups_trun.groups.items():
                 df_se_data_trun.loc[label_idx, feat] = df_se_data_trun.loc[label_idx, feat].fillna(
--- app/lib/__init__.py
+++ app/lib/__init__.py
@@ -1,4 +1,4 @@
 from .bokeh_viz_chartdata import create_bokeh_viz_chartdata
 from .bokeh_viz_ser_risk import create_bokeh_viz_ser_risk
-from .ser_risk import predict_ser_risk
+from .ser_risk import predict_ser_risk, impute_values, compute_total_gcs, compute_rsbi_ve, compute_ser_features, get_ser_analysis_df
 from .create_cohorts import get_se_cohort, get_chart_data, extract_wfdb_numerics
